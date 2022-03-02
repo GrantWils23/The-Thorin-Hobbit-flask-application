935ca43e6c84cfd90087377ac375158f9c6c6cac
Grant <GrantWils.23@googlemail.com>
2022-03-02 21:43:25 +0000

    add advance routing

--- run.py
+++ run.py
@@ -23,6 +23,18 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    '''this func will render the pages of the members from about.html'''
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
 @app.route("/contact")
 def contact():
     '''this func renders the contact.html file from the templates'''
